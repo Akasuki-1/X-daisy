6ce9a30a017d2723083b43d4b50fe5592448d273
Akasuki-1 <79846938+Akasuki-1@users.noreply.github.com>
2021-03-18 09:29:13 +0000

    Update __main__.py

--- DaisyX/__main__.py
+++ DaisyX/__main__.py
@@ -363,8 +363,8 @@ def help_button(update, context):
 def mareek_about_callback(update, context):
     query = update.callback_query
     if query.data == "mareek_":
-        query.message.edit_text(
-            text= "LOCAL_IMG",
+        query.message.edit_sticker(
+            LOCAL_IMG,
             parse_mode=ParseMode.HTML,
             disable_web_page_preview=True,
             reply_markup=InlineKeyboardMarkup(
