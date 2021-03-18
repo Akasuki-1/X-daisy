1b58e9888581b957752964128837d8c0dceb6466
Akasuki-1 <79846938+Akasuki-1@users.noreply.github.com>
2021-03-18 10:18:11 +0000

    Update meme.py

--- DaisyX/modules/meme.py
+++ DaisyX/modules/meme.py
@@ -529,7 +529,7 @@ All regex filters can be disabled incase u don't want... like: `/disable goodnig
 
 """
 
-__mod_name__ = "Memes"
+__mod_name__ = "Mᴇᴍᴇꜱ"
 
 
 PAT_HANDLER = DisableAbleCommandHandler("pat", pat)
