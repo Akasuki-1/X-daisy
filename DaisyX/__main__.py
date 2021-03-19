30346efc0b174b8862011e06d2514a21d2fafc95
Akasuki-1 <79846938+Akasuki-1@users.noreply.github.com>
2021-03-19 21:06:04 +0530

    Update __main__.py

--- DaisyX/__main__.py
+++ DaisyX/__main__.py
@@ -63,6 +63,33 @@ from DaisyX.modules.helper_funcs.chat_status import is_user_admin
 from DaisyX.modules.helper_funcs.misc import paginate_modules
 from DaisyX.modules.helper_funcs.readable_time import get_readable_time
 
+import logging
+
+import pyrogram
+# the logging things
+from DaisyX import AUTH_CHANNEL
+
+logging.basicConfig(
+    level=logging.DEBUG,
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+)
+logging.getLogger("pyrogram").setLevel(logging.WARNING)
+LOGGER = logging.getLogger(__name__)
+
+
+async def new_join_f(client, message):
+    chat_type = message.chat.type
+    if chat_type != "private":
+        await message.reply_text(f"Current CHAT ID: <code>{message.chat.id}</code>")
+        # leave chat
+        await client.leave_chat(
+            chat_id=message.chat.id,
+            delete=True
+        )
+    # delete all other messages, except for AUTH_CHANNEL
+    await message.delete(revoke=True)
+
+
 PM_START_TEXT = """
 Hello there, I'm a big fan of Kiccha Sudeep [,](https://telegra.ph/file/4f2039d807a346f6baf69.jpg) so my name is *kicchafan*.
 
